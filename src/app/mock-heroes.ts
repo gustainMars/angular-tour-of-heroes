0792555911d500298f40a2ef1e75b60fca94fa7f
Gustavo Marques <gmartes23@gmail.com>
2021-08-16 17:24:15 -0300

    Lista de Heróis criado constante com array de Hero, usado ngFor no template para iterar sobre HEROES, definido valor de heroes recebe constante HEROES

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,12 @@
+import { Hero } from "./hero";
+
+export const HEROES: Hero[] = [
+  { id: 1, name: 'Levi Ackerman' },
+  { id: 2, name: 'Eren Yeager' },
+  { id: 3, name: 'Grisha Yeager' },
+  { id: 4, name: 'Mikasa Ackerman' },
+  { id: 5, name: 'Erwin Smith' },
+  { id: 6, name: 'Armin Arlert' },
+  { id: 7, name: 'Connie Springer' },
+  { id: 8, name: 'Sasha Braus' },
+];
